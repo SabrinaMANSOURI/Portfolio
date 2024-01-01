3ffa372d39373a90be451700a220d94ea86da79a
SabrinaMANSOURI <131768058+SabrinaMANSOURI@users.noreply.github.com>
2024-01-01 14:13:56 +0100

    last_changes2

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*typed js */
 const typed = new Typed('.multiple-text', {
-    strings: ['Full stack Developer', 'Mobile Developer', 'Networking Student', 'UI / UX Designer',],
+    strings: ['Web Developer', 'Mobile Developer', 'Networking Student', 'UI / UX Designer',],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
